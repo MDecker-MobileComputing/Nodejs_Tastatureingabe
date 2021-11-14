ebf4ee3ab005acc17eda8495933d131191e3fc67
MDecker-MobileComputing <mide_42b@yahoo.de>
2021-11-14 13:50:17 +0100

    Kommentar in js-Datei verbessert.

--- Tastatureingabe_StringlaengeAusgeben.js
+++ Tastatureingabe_StringlaengeAusgeben.js
@@ -3,10 +3,13 @@
 /*
  * Node.js-Programm zum Einlesen von Texteingaben von der Tastatur.
  * Hierfür wird das Modul "readline-sync" verwendet:
- * https://github.com/anseki/readline-sync 
+ * https://github.com/anseki/readline-sync
  *
  * Installation: npm install readline-sync
- * 
+ *
+ * Da das Modul "readline-sync" in der Datei "package.json" als Abhängigkeit
+ * eingetragen ist, reicht auch der folgende Befehl: npm install
+ *
  * This file is licensed under the terms of the BSD 3-Clause License.
  */
 
